7a662093d027f0f530f28ace2bbb0fd21c305d95
phmiranda <pehhagah.1607@gmail.com>
2020-05-05 12:49:36 -0300

    [add] - criação do script de inserção de dados necessários para popular a base de dados para consumir a API.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,35 @@
+/* comentário */
+INSERT INTO usuario(id, nome, email, senha) VALUES(1, 'Aluno 01', 'aluno.primeiro@email.com.br', '123456');
+INSERT INTO usuario(id, nome, email, senha) VALUES(2, 'Aluno 02', 'aluno.segundo@email.com.br', '123456');
+INSERT INTO usuario(id, nome, email, senha) VALUES(3, 'Aluno 03', 'aluno.terceiro@email.com.br', '123456');
+INSERT INTO usuario(id, nome, email, senha) VALUES(4, 'Aluno 04', 'aluno.quarto@email.com.br', '123456');
+INSERT INTO usuario(id, nome, email, senha) VALUES(5, 'Aluno 05', 'aluno.quinto@email.com.br', '123456');
+
+/* comentário */
+INSERT INTO curso(id, nome, categoria) VALUES(1, 'Curso #01', 'Categoria 01');
+INSERT INTO curso(id, nome, categoria) VALUES(2, 'Curso #02', 'Categoria 02');
+INSERT INTO curso(id, nome, categoria) VALUES(3, 'Curso #02', 'Categoria 03');
+INSERT INTO curso(id, nome, categoria) VALUES(4, 'Curso #02', 'Categoria 04');
+INSERT INTO curso(id, nome, categoria) VALUES(5, 'Curso #02', 'Categoria 05');
+
+/* comentário */
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(1, 'Dúvida #01', 'Título 01', '2019-05-05 18:00:00', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(2, 'Dúvida #02', 'Título 02', '2019-05-05 19:00:00', 'NAO_RESPONDIDO', 3, 2);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(3, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 5, 3);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(4, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 2, 4);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(5, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 4, 5);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(6, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 5);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(7, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 3, 5);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(8, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 5, 3);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(9, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 2, 2);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(10, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 4, 1);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(11, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 4);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(12, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 3, 5);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(13, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 5, 5);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(14, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 2, 3);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(15, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 4, 3);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(16, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(17, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 3, 2);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(18, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 5, 1);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(19, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 2, 1);
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES(20, 'Dúvida #03', 'Título 03', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 4, 3);
