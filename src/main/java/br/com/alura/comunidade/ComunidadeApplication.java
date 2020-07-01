179e80857a11dbaf85b7aeff942179c5bd013196
phmiranda <pehhagah.1607@gmail.com>
2020-07-01 07:54:30 -0300

    [update] - atualização no nome da classe de inicialização e testes do Spring Boot.

--- src/main/java/br/com/alura/comunidade/ComunidadeApplication.java
+++ src/main/java/br/com/alura/comunidade/ComunidadeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ForumApplication {
+public class ComunidadeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ForumApplication.class, args);
+		SpringApplication.run(ComunidadeApplication.class, args);
 	}
 
 }
