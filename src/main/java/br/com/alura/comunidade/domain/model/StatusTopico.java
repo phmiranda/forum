02d1d82843e0b9f6a73ff1827dfd3e975c25b860
phmiranda <pehhagah.1607@gmail.com>
2021-04-29 16:18:08 -0300

    [create] - criação da camada de validação e regras de negócios.

---
+++ src/main/java/br/com/alura/comunidade/domain/model/StatusTopico.java
@@ -0,0 +1,15 @@
+/**
+ * User: phmiranda
+ * Project: comunidade
+ * Description: this class execute...!
+ * Date: 23/12/2020
+ */
+
+package br.com.alura.comunidade.entity.model;
+
+public enum StatusTopico {
+	NAO_RESPONDIDO,
+	NAO_SOLUCIONADO,
+	SOLUCIONADO,
+	FECHADO;
+}
---
+++ src/main/java/br/com/alura/comunidade/service/TopicoService.java
@@ -0,0 +1,12 @@
+/**
+ * User: Pedro
+ * Project: comunidade
+ * Description: this class execute...!
+ * Date: 29/04/2021
+ */
+
+package br.com.alura.comunidade.service;
+
+public class TopicoService {
+    // comment
+}
