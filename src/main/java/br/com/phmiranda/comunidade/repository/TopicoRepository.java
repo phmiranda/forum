94b690e44bb8212cdc871964944161fe3c0bafed
phmiranda <pehhagah.1607@gmail.com>
2021-08-06 19:35:14 -0300

    [add] - inclusão de linhas para persistência.
    [add] - inclusão de linhas para persistência dos dados.

--- src/main/java/br/com/phmiranda/comunidade/repository/TopicoRepository.java
+++ src/main/java/br/com/phmiranda/comunidade/repository/TopicoRepository.java
@@ -9,12 +9,12 @@
 package br.com.phmiranda.comunidade.repository;
 
 import br.com.phmiranda.comunidade.domain.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
-    List<Topico> findByCursoNome(String nomeCurso);
+    Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 }
